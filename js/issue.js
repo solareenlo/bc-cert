678c8eebe95518e05c4d8b06e99b68d7ace766ba
solareenlo <solareenlo@gmail.com>
2019-04-04 07:13:04 +0900

    scriptを別ファイルに移動

--- js/issue.js
+++ js/issue.js
@@ -6,6 +6,19 @@ const TESTNET = bitcoin.networks.testnet;
 
 const getDigest = (fileBuffeArrray) => bitcoin.crypto.sha256(Buffer(fileBuffeArrray)).toString('hex');
 
+const makeDigest = (evt) => {
+  document.getElementById('digest').value = '';
+  const files = evt.target.files[0];
+  const reader = new FileReader();
+  reader.onload = (function(){
+    return function(e){
+      document.getElementById('digest').value = getDigest(e.target.result);
+      document.getElementById('digest').textContent = getDigest(e.target.result);
+    }
+  })(files);
+  reader.readAsArrayBuffer(files);
+}
+
 const makeAddress = (wif) => {
   const keyPair = bitcoin.ECPair.fromWIF(wif);
   const { address } = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey, network: TESTNET });
@@ -13,4 +26,5 @@ const makeAddress = (wif) => {
 }
 
 window.getDigest = getDigest;
+window.makeDigest = makeDigest;
 window.makeAddress = makeAddress;
