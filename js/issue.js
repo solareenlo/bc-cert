22deba788b910ade7706044da05ffe639b117bbc
solareenlo <solareenlo@gmail.com>
2019-04-05 17:43:21 +0900

    http request を同期処理してutxoの残高取得

--- js/issue.js
+++ js/issue.js
@@ -1,6 +1,7 @@
 const bip39 = require('bip39');
 const bitcoin = require('bitcoinjs-lib');
 const https = require('https');
+const request = require('request');
 const wif = require('wif');
 
 const TESTNET = bitcoin.networks.testnet;
@@ -21,33 +22,60 @@ const makeDigest = (evt) => {
   reader.readAsArrayBuffer(files);
 }
 
-const makeAddress = (mnemonic) => {
+const makeAddress = async (mnemonic) => {
   const seed = bip39.mnemonicToSeed(mnemonic);
   const xprv = bitcoin.bip32.fromSeed(seed, TESTNET);
   const p2pkh = bitcoin.payments.p2pkh({ pubkey: xprv.publicKey, network: TESTNET });
   const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: xprv.publicKey, network: TESTNET });
   const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: TESTNET });
   document.getElementById('address').textContent = p2pkh.address;
-  getBalance(p2pkh.address);
+  const balance = await getBalance(p2pkh.address);
+  document.getElementById('balance').textContent = balance;
 }
 
-const getBalance = (address) => {
+function httpRequest(options) {
+  return new Promise((resolve, reject) => {
+    request(options, (error, res, body) => {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+const getBalance = async (address) => {
+  let balance = 0;
   // const URL = `https://chain.so/api/v2/get_address_balance/BTCTEST/${address}`;
   const URL = `https://chain.so/api/v2/get_tx_unspent/BTCTEST/${address}`;
-  let balance;
-  https.get(URL, (res) => {
+  const requestOptions = {
+    url: URL,
+    method: 'GET'
+  }
+  const body = await httpRequest(requestOptions);
+  let json = JSON.parse(body);
+  json.data.txs = json.data.txs.map((tx) => {tx.value=Math.floor(100000000*tx.value); return tx});
+  balance = json.data.txs.reduce((a, b) => a+b.value, 0);
+  /* この中では非同期処理でhttpリクエストを投げてる.
+   * Nodejsは標準で非同期処理してくれる.
+  await https.get(URL, (res) => {
     let data = [];
     res.on('data', (d) => {
       data.push(d);
     }).on('end', () => {
+      console.log('balance0: ', balance);
       const json = JSON.parse(Buffer.concat(data).toString());
       json.data.txs = json.data.txs.map((tx) => {tx.value=Math.floor(100000000*tx.value); return tx});
       balance = json.data.txs.reduce((a, b) => a+b.value, 0);
-      document.getElementById('balance').textContent = balance;
+      console.log('balance1: ', balance);
+      return balance;
     });
   }).on('error', (e) => {
     console.error(e);
   });
+  */
+      return balance;
 }
 
 const broadcastTx = (mnemonic, digest) => {
