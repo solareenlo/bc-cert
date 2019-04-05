08fcc4a084812d3d02a8b3a2a67e2169d7c4af66
solareenlo <solareenlo@gmail.com>
2019-04-06 03:32:27 +0900

    Tx作成を実装

--- js/issue.js
+++ js/issue.js
@@ -22,22 +22,38 @@ const makeDigest = (evt) => {
   reader.readAsArrayBuffer(files);
 }
 
-const getAddress = async (mnemonic) => {
+const generatePayment = (mnemonic, NET) => {
   const seed = bip39.mnemonicToSeed(mnemonic);
-  const xprv = bitcoin.bip32.fromSeed(seed, TESTNET);
-  const p2pkh = bitcoin.payments.p2pkh({ pubkey: xprv.publicKey, network: TESTNET });
-  const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: xprv.publicKey, network: TESTNET });
-  const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: TESTNET });
-  document.getElementById('address').textContent = p2pkh.address;
-  const balance = await getBalance(p2pkh.address);
-  document.getElementById('balance').textContent = balance;
+  const xprv = bitcoin.bip32.fromSeed(seed, NET);
+  const p2pkh = bitcoin.payments.p2pkh({ pubkey: xprv.publicKey, network: NET });
+  const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: xprv.publicKey, network: NET });
+  const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: NET });
+  return p2pkh;
 }
 
-function httpRequest(options) {
+const generateKeypair = (mnemonic, NET) => {
+  const seed = bip39.mnemonicToSeed(mnemonic);
+  const xprv = bitcoin.bip32.fromSeed(seed, NET);
+  const privateKey = xprv.derive(0).privateKey;
+  const keyPair = bitcoin.ECPair.fromPrivateKey(privateKey, { network: NET });
+  return keyPair;
+}
+
+const getAddress = async (mnemonic) => {
+  const payment = generatePayment(mnemonic, TESTNET);
+  document.getElementById('address').textContent = payment.address;
+  document.getElementById('address').value = payment.address;
+  const balance = await getBalance(payment.address);
+  document.getElementById('balance').textContent = balance;
+  document.getElementById('balance').value = balance;
+}
+
+function doRequest(options) {
   return new Promise((resolve, reject) => {
     request(options, (error, res, body) => {
       if (!error && res.statusCode == 200) {
-        resolve(body);
+        const json = JSON.parse(body);
+        resolve(json);
       } else {
         reject(error);
       }
@@ -51,17 +67,70 @@ const getBalance = async (address) => {
     url: URL,
     method: 'GET'
   }
-  const body = await httpRequest(requestOptions);
-  let json = JSON.parse(body);
+  const json = await doRequest(requestOptions);
   json.data.txs = json.data.txs.map((tx) => {tx.value=Math.floor(100000000*tx.value); return tx});
   let balance = json.data.txs.reduce((a, b) => a+b.value, 0);
   return balance;
 }
 
-const broadcastTx = (mnemonic, digest) => {
-  document.getElementById('txId').textContent = 'testtesttest';
+const createTx = async (mnemonic, address, digest, balance) => {
+  const fee = 1000;
+  const txb = new bitcoin.TransactionBuilder(TESTNET);
+  const payment = generatePayment(mnemonic, TESTNET);
+  const keyPair = generateKeypair(mnemonic, TESTNET);
+  // get utxo
+  const URL = `https://chain.so/api/v2/get_tx_unspent/BTCTEST/${address}`;
+  const requestOptions = {
+    url: URL,
+    method: 'GET'
+  }
+  const json = await doRequest(requestOptions);
+  json.data.txs = json.data.txs.map((tx) => {tx.value=Math.floor(100000000*tx.value); return tx});
+  // OP_RETURNの後ろにpdfのハッシュ値をくっ付けてる
+  const data = Buffer.from(digest, 'hex');
+  const certificateOutput = bitcoin.payments.embed({ data: [data] }).output;
+  // add Input
+  for(let utxo of json.data.txs) {
+    // p2pkh用のInput
+    txb.addInput(utxo.txid, utxo.output_no);
+    // p2wpkh用のInput
+    // txb.addInput(utxo.txid, utxo.output_no, null, payment.output);
+  }
+  // add Output
+  txb.addOutput(certificateOutput, 0); // pdfのハッシュ値を載せたOP_RETURN用のoutput
+  txb.addOutput(address, balance - fee); // 残高をまた自分のアドレスに返却
+  // sign
+  for(let i in json.data.txs) {
+    // p2pkh用の署名
+    txb.sign(+i, keyPair);
+    // p2wpkh用の署名
+    // txb.sign(+i, keyPair, null, null, json.data.txs[i].value);
+  }
+  // build
+  const tx = txb.build();
+  document.getElementById('rawTx').textContent = tx.toHex();
+  document.getElementById('rawTx').value = tx.toHex();
+  document.getElementById('txId').textContent = tx.getId();
+  document.getElementById('txId').value = tx.getId();
+}
+
+const broadcastTx = (rawTx) => {
+  const URL = 'https://chain.so/api/v2/send_tx/BTCTEST';
+  const requestOptions = {
+    url: URL,
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    json: {
+      'tx_hex': rawTx
+    }
+  }
+  // const json = await doRequest(requestOptions);
+  document.getElementById('tx_id').textContent = json.txid;
 }
 
 window.makeDigest = makeDigest;
 window.getAddress = getAddress;
+window.createTx = createTx;
 window.broadcastTx = broadcastTx;
