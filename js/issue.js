c3e8ad65c1c4c84406b874179eb8125cea32480e
solareenlo <solareenlo@gmail.com>
2019-04-05 09:26:50 +0900

    残高確認を実装

--- js/issue.js
+++ js/issue.js
@@ -24,8 +24,30 @@ const makeDigest = (evt) => {
 const makeAddress = (mnemonic) => {
   const seed = bip39.mnemonicToSeed(mnemonic);
   const xprv = bitcoin.bip32.fromSeed(seed, TESTNET);
-  const { address } = bitcoin.payments.p2wpkh({ pubkey: xprv.publicKey, network: TESTNET });
-  document.getElementById('address').textContent = address;
+  const p2pkh = bitcoin.payments.p2pkh({ pubkey: xprv.publicKey, network: TESTNET });
+  const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: xprv.publicKey, network: TESTNET });
+  const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: TESTNET });
+  document.getElementById('address').textContent = p2pkh.address;
+  getBalance(p2pkh.address);
+}
+
+const getBalance = (address) => {
+  // const URL = `https://chain.so/api/v2/get_address_balance/BTCTEST/${address}`;
+  const URL = `https://chain.so/api/v2/get_tx_unspent/BTCTEST/${address}`;
+  let balance;
+  https.get(URL, (res) => {
+    let data = [];
+    res.on('data', (d) => {
+      data.push(d);
+    }).on('end', () => {
+      const json = JSON.parse(Buffer.concat(data).toString());
+      json.data.txs = json.data.txs.map((tx) => {tx.value=Math.floor(100000000*tx.value); return tx});
+      balance = json.data.txs.reduce((a, b) => a+b.value, 0);
+      document.getElementById('balance').textContent = balance;
+    });
+  }).on('error', (e) => {
+    console.error(e);
+  });
 }
 
 const broadcastTx = (mnemonic, digest) => {
--- js/issue_bundle.js
+++ js/issue_bundle.js
@@ -6,6 +6,7 @@ const https = require('https');
 const wif = require('wif');
 
 const TESTNET = bitcoin.networks.testnet;
+const MAINNET = bitcoin.networks.bitcoin;
 
 const getDigest = (fileBuffeArrray) => bitcoin.crypto.sha256(Buffer(fileBuffeArrray)).toString('hex');
 
@@ -25,11 +26,33 @@ const makeDigest = (evt) => {
 const makeAddress = (mnemonic) => {
   const seed = bip39.mnemonicToSeed(mnemonic);
   const xprv = bitcoin.bip32.fromSeed(seed, TESTNET);
-  const { address } = bitcoin.payments.p2wpkh({ pubkey: xprv.publicKey, network: TESTNET });
-  document.getElementById('address').textContent = address;
+  const p2pkh = bitcoin.payments.p2pkh({ pubkey: xprv.publicKey, network: TESTNET });
+  const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: xprv.publicKey, network: TESTNET });
+  const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: TESTNET });
+  document.getElementById('address').textContent = p2pkh.address;
+  getBalance(p2pkh.address);
 }
 
-const broadcastTx = (wif, digest) => {
+const getBalance = (address) => {
+  // const URL = `https://chain.so/api/v2/get_address_balance/BTCTEST/${address}`;
+  const URL = `https://chain.so/api/v2/get_tx_unspent/BTCTEST/${address}`;
+  let balance;
+  https.get(URL, (res) => {
+    let data = [];
+    res.on('data', (d) => {
+      data.push(d);
+    }).on('end', () => {
+      const json = JSON.parse(Buffer.concat(data).toString());
+      json.data.txs = json.data.txs.map((tx) => {tx.value=Math.floor(100000000*tx.value); return tx});
+      balance = json.data.txs.reduce((a, b) => a+b.value, 0);
+      document.getElementById('balance').textContent = balance;
+    });
+  }).on('error', (e) => {
+    console.error(e);
+  });
+}
+
+const broadcastTx = (mnemonic, digest) => {
   document.getElementById('txId').textContent = 'testtesttest';
 }
 
